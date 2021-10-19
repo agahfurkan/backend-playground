4ce6bbfe7b817746e9411064e5fa8a7c8d98c576
agahfurkan <agahfurkan@gmail.com>
2021-10-19 23:49:20 +0300

    enable swagger on all releases

--- ApiPlayground/Startup.cs
+++ ApiPlayground/Startup.cs
@@ -27,7 +27,7 @@ namespace ApiPlayground
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo {Title = "ApiPlayground", Version = "v1"});
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "ApiPlayground", Version = "v1" });
                 c.OperationFilter<AddRequiredHeaderParameter>();
             });
             services.AddDbContext<DbContextClass>(options =>
@@ -57,12 +57,9 @@ namespace ApiPlayground
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => { c.SwaggerEndpoint("/swagger/v1/swagger.json", "ApiPlayground v1"); });
-            }
+            app.UseDeveloperExceptionPage();
+            app.UseSwagger();
+            app.UseSwaggerUI(c => { c.SwaggerEndpoint("/swagger/v1/swagger.json", "ApiPlayground v1"); });
 
             app.UseHttpsRedirection();
 
